011e83280abe987d533e042a1bbff51aba8d17e9
anshuldhuran <108670612+anshuldhuran@users.noreply.github.com>
2022-12-08 23:23:50 +0530

    second

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
+import Header from '../components/Header'
+import { Footer } from '../components/Footer'
+import ScrollProgress from '../components/ScrollProgress'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+  <Header />
+  
+  <Component {...pageProps} />
+  <Footer/>
+  </>
 }
 
 export default MyApp
